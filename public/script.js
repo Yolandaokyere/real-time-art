6ad9ab62307ad9432b7a1d273e0275acc93879f5
Yolandaokyere <yolandaokyere@outlook.com>
2022-06-20 11:50:24 +0200

    :art: loopen door [selectedWord] update letters

--- public/script.js
+++ public/script.js
@@ -12,60 +12,72 @@ let pogingen = 0;
 // WORD ARRAY
 
 // Fetchen naar de nieuwe route '/words'
-fetch('/words').then(data => data.json()).then(data => {
-  // data is een object. in het object data, zit een property called data. Die geven we een andere naam, words
-  var {data: words} = data;
-  
-  
-  // Pick a random word out of the array of words
-  var selectedWord = words[Math.floor(Math.random() * words.length)];
-  console.log(selectedWord);
-  
-  // Nu wordt er door het woord (selectedWord.length) geloopt. Voor elk letter van het woord zet het een ' _ '
-  var answerArray = [];
-  for (var i = 0; i < selectedWord.length; i++) {
-    answerArray[i] = "_";
-  }
-  
-  var remainingLetters = selectedWord.length;
-  let answer;
-  let lives;
-  
-  // ------------------------ T H E G A M E ---------------------------
-  document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    socket.emit("message", input.value);
-    console.log(input.value);
-    
-    if (selectedWord.includes(input.value)) {
-      console.log("letter geraden!");
-      console.log(selectedWord.indexOf(input.value));
-      // Answerarray updaten
-      answerArray[selectedWord.indexOf(input.value)] = input.value;
-      console.log(answerArray);
-    } else {
-      // Gebruiker heeft het fout we gaan pogingen updaten
-      if (pogingen !== 6) {
-        pogingen = pogingen + 1;
-        if (parts.length > 0) {
-          var part = document.getElementById(parts[0]);
-          part.style.display = "block";
-          parts.shift();
-          console.log("fout!");
-        }
-      } else {
-        alert("Game Over");
-      }
+fetch("/words")
+  .then((data) => data.json())
+  .then((data) => {
+    // data is een object. in het object data, zit een property called data. Die geven we een andere naam, words
+    var { data: words } = data;
+
+    // Pick a random word out of the array of words
+    var selectedWord = words[Math.floor(Math.random() * words.length)];
+    console.log(selectedWord);
+
+    // Nu wordt er door het woord (selectedWord.length) geloopt. Voor elk letter van het woord zet het een ' _ '
+    var answerArray = [];
+    for (var i = 0; i < selectedWord.length; i++) {
+      answerArray[i] = "_";
     }
-  });
-})
-  
-  socket.on("message", (message) => {
-    input.value = "";
-    messages.appendChild(
-      Object.assign(document.createElement("li"), { textContent: message })
-      );
-      messages.scrollTop = messages.scrollHeight;
+
+    var remainingLetters = selectedWord.length;
+    let answer;
+    let lives;
+
+    // ------------------------ T H E G A M E ---------------------------
+    document.querySelector("form").addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      socket.emit("message", input.value);
+      console.log(input.value);
+
+      // Het woord in een array plaatsen
+      const selectedWordArray = selectedWord.split("");
+      //b
+      // ['t','e','s','t']
+
+
+      // Loopen door het woord
+      for (let i = 0; i < selectedWordArray.length; i++) {
+        // als het geraden letter voorkomt in het woord, update answerarray
+        if (input.value === selectedWordArray[i]) {
+          console.log("letter geraden!");
+          // Answerarray updaten
+          answerArray[i] = input.value;
+          console.log(answerArray);
+        }
+      }
+
+      // Fout antwoord
+      if (!selectedWord.includes(input.value)) {
+        // Gebruiker heeft het fout we gaan pogingen updaten
+        if (pogingen !== 6) {
+          pogingen = pogingen + 1;
+          if (parts.length > 0) {
+            var part = document.getElementById(parts[0]);
+            part.style.display = "block";
+            parts.shift();
+            console.log("fout!");
+          }
+        } else {
+          alert("Game Over");
+        }
+      }
     });
-    
+  });
+
+socket.on("message", (message) => {
+  input.value = "";
+  messages.appendChild(
+    Object.assign(document.createElement("li"), { textContent: message })
+  );
+  messages.scrollTop = messages.scrollHeight;
+});
