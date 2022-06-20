c659f05198170ae334e4e4f1b29cf475ed876243
Yolandaokyere <yolandaokyere@outlook.com>
2022-06-20 11:24:08 +0200

    :rocket: movie API

--- main.js
+++ main.js
@@ -2,17 +2,45 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const http = require('http').createServer(app)
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const port = process.env.PORT || 6060;
 const io = require('socket.io')(http)
+// THE MOVIE DATABASE API
+const API_MOVIEDB_KEY = process.env.API_KEY
+const RESULTS_PAGES = 1
+const endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${API_MOVIEDB_KEY}` 
+// const urlendpointt}
+
+const router = new express.Router() // Extra router om data te sturen naar script.js
+console.log(endpoint);
+
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
+app.use(router); //Vertel Express dat we een extra router willen gebruiken
 app.use(express.static("public"));
 
-app.get("/", function (request, response) {
-    response.render('index')
+// Nieuwe endpoints '/words' die titles verstuurd naar de client
+router.get('/words', (req, res) => {
+  // Fetch data
+  fetch(endpoint)
+  .then(res => res.json())
+  .then(json => {
+ 
+ const {results} = json;
+//  map functie returned een nieuwe array met alleen de titles
+ const titles = results.map(result => result.title)
+ 
+//  Verstuur de data (titles) naar script.js
+ res.send({data: titles})
+})
 });
+
+app.get("/", function (request, response) {
+  //  stuurt titlesdata naar de client
+ response.render('index')  
+  })
+
 
 io.on('connection', (socket) => {
     console.log('a user (socket.io-client) is connected')
--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let socket = io();
 let messages = document.querySelector("section ul");
 let input = document.querySelector("input");
-var form = document.getElementById('form');
+var form = document.getElementById("form");
 // THE HANGMAN CHAT GAME
 // - - - - - - - - - - CONSTANTES - - - - - - - - - - - - -
 
@@ -10,55 +10,62 @@ var parts = ["head", "torso", "arm-l", "arm-r", "leg-l", "leg-r"];
 let pogingen = 0;
 
 // WORD ARRAY
-var words = ["test", "cheese", "dog"];
 
-// Pick a random word out of the array of words
-var selectedWord = words[Math.floor(Math.random() * words.length)];
-console.log(selectedWord);
-
-// Nu wordt er door het woord (selectedWord.length) geloopt. Voor elk letter van het woord zet het een ' _ '
-var answerArray = [];
-for (var i = 0; i < selectedWord.length; i++) {
-  answerArray[i] = "_";
-}
-
-var remainingLetters = selectedWord.length;
-let answer;
-let lives;
-
-// ------------------------ T H E G A M E ---------------------------
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
+// Fetchen naar de nieuwe route '/words'
+fetch('/words').then(data => data.json()).then(data => {
+  // data is een object. in het object data, zit een property called data. Die geven we een andere naam, words
+  var {data: words} = data;
   
-  socket.emit("message", input.value);
-  console.log(input.value);
-
-  if (selectedWord.includes(input.value)) {
-    console.log("letter geraden!");
-    console.log(selectedWord.indexOf(input.value));
-    // Answerarray updaten
-    answerArray[selectedWord.indexOf(input.value)] = input.value;
-    console.log(answerArray);
-  } else {
-    // Gebruiker heeft het fout we gaan pogingen updaten
-    if (pogingen !== 6) {
-      pogingen = pogingen + 1;
-      if (parts.length > 0) {
-        var part = document.getElementById(parts[0]);
-        part.style.display = "block";
-        parts.shift();
-        console.log("fout!");
-      }
-    } else {
-      alert("Game Over");
-    }
+  
+  // Pick a random word out of the array of words
+  var selectedWord = words[Math.floor(Math.random() * words.length)];
+  console.log(selectedWord);
+  
+  // Nu wordt er door het woord (selectedWord.length) geloopt. Voor elk letter van het woord zet het een ' _ '
+  var answerArray = [];
+  for (var i = 0; i < selectedWord.length; i++) {
+    answerArray[i] = "_";
   }
-});
-
-socket.on("message", (message) => {
-  input.value = ''
-  messages.appendChild(
-    Object.assign(document.createElement("li"), { textContent: message })
-  );
-  messages.scrollTop = messages.scrollHeight;
-});
+  
+  var remainingLetters = selectedWord.length;
+  let answer;
+  let lives;
+  
+  // ------------------------ T H E G A M E ---------------------------
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    socket.emit("message", input.value);
+    console.log(input.value);
+    
+    if (selectedWord.includes(input.value)) {
+      console.log("letter geraden!");
+      console.log(selectedWord.indexOf(input.value));
+      // Answerarray updaten
+      answerArray[selectedWord.indexOf(input.value)] = input.value;
+      console.log(answerArray);
+    } else {
+      // Gebruiker heeft het fout we gaan pogingen updaten
+      if (pogingen !== 6) {
+        pogingen = pogingen + 1;
+        if (parts.length > 0) {
+          var part = document.getElementById(parts[0]);
+          part.style.display = "block";
+          parts.shift();
+          console.log("fout!");
+        }
+      } else {
+        alert("Game Over");
+      }
+    }
+  });
+})
+  
+  socket.on("message", (message) => {
+    input.value = "";
+    messages.appendChild(
+      Object.assign(document.createElement("li"), { textContent: message })
+      );
+      messages.scrollTop = messages.scrollHeight;
+    });
+    
